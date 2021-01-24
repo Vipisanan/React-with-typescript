8af2e429927cdaf6335e34ff1733cca9db99a4d1
vipisanan <vipisanan1@gmail.com>
2021-01-24 19:53:59 +0530

    added tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>vipi</h1>
+          <input className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block appearance-none leading-normal m-10" type="email" placeholder="jane@example.com" />
+
       </header>
     </div>
   );
